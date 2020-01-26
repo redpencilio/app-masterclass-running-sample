c1ce16434fc6dea1362c8dcb86c66f584d527542
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-26 22:47:34 +0100

    added books-service

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -2,5 +2,13 @@ defmodule Dispatcher do
   use Matcher
   define_accept_types []
 
+  match "/books/*path", _ do
+    forward conn, path, "http://books-service/books/"
+  end
+
+  match "/authors/*path", _ do
+    forward conn, path, "http://books-service/authors/"
+  end
+
   last_match
 end
